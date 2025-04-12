7ce0f77a6856251478951ff954fcece1285a5464
shajid <shajid@gmail.com>
2025-04-12 18:03:24 +0530

    static files

--- AI/urls.py
+++ AI/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import InvokeAgentView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("invoke/", InvokeAgentView.as_view(), name="invoke"),
 ]
+
+urlpatterns.append(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
--- AroBot/settings.py
+++ AroBot/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config,Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
