e5e6157e4a0b31576eac8ae6faa58c64d467f5d5
shajid <shajid@gmail.com>
2025-04-12 21:29:01 +0530

    url patterns

--- AI/urls.py
+++ AI/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from .views import InvokeAgentView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("invoke/", InvokeAgentView.as_view(), name="invoke"),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
